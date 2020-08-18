4e4bd0e6fee89637cb8fa90cf09cc2f0c1d59a1d
GummiCode <gummicodeblog@gmail.com>
2020-08-18 06:55:01 +0100

    Removed the original Splash component. Replaced it with the new Splash2 component, renamed to Splash.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyles from '../styles/GlobalStyles';
 import Navbar from './Navbar/Navbar';
 import Splash from './Splash';
-import Splash2 from './Splash2';
+import Splash2 from './Splash';
 import Listing from './Listing';
 import AllListings from './AllListings/AllListings';
 import NewListing from './NewListing';
