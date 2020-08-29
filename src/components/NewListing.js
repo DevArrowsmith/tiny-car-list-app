a2b402876d2af005a6c5f23ad3ff622fc361c0e1
GummiCode <gummicodeblog@gmail.com>
2020-08-29 17:26:34 +0100

    Modified the styling of the NewListing component for small mobile. Commencing mobile-first responsive styling.

--- src/components/NewListing.js
+++ src/components/NewListing.js
@@ -1,24 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import postListing from '../controllers/newListingController';
-
-const NewListingContainer = styled.div`
-  height: calc(100vh - 120px - 20px);
-  width: 100vw;
-  margin: 120px 0 0 0;
-  padding: 20px 0 0 0;
-  color: white;
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  justify-content: flex-start;
-`;
-
-const Intro = styled.p`
-  width: calc(100% - 40px);
-  max-width: 320px;
-  margin: 0 20px 15px 20px;
-`;
+import road5 from '../assets/images/roads/road5.png';
 
 const Header = styled.div`
   position: fixed;
@@ -27,28 +10,60 @@ const Header = styled.div`
   height: 60px;
   width: 100vw;
   background: black;
-  border-bottom: 1px solid #454545;
   display: flex;
   flex-flow: row nowrap;
   justify-content: space-between;
   align-items: center;
+  z-index: 1;
+
+  span {
+    height: 60px;
+    margin: 0 0 0 20px;
+    font-size: 1.5em;
+    font-weight: bold;
+    color: white;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
-const HeaderText = styled.span`
-  height: 60px;
-  margin: 0 0 0 20px;
-  border-bottom: 1px solid #454545;
-  font-size: 1.5em;
-  font-weight: bold;
+const NewListingContainer = styled.div`
+  min-height: calc(100vh - 120px);
+  width: 100vw;
+  margin: 120px 0 0 0;
   color: white;
+  background-image: url(${road5});
+  background-position: bottom;
+  background-size: cover;
   display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const MainContainer = styled.div`
+  min-height: calc(100vh - 120px - 20px);
+  width: 100vw;
+  max-width: 360px;
+  padding: 20px 0 0 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(2px);
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
   align-items: center;
 `;
 
+const Intro = styled.p`
+  width: calc(100% - 40px);
+  max-width: 320px;
+  margin: 0 20px 15px 20px;
+`;
+
 const FormStyles = styled.form`
-  width: 320px;
+  width: 280px;
   display: flex;
   flex-flow: column nowrap;
   justify-content: start;
@@ -56,6 +71,7 @@ const FormStyles = styled.form`
 `;
 
 const FormElement = styled.div`
+  width: 280px;
   display: flex;
   flex-flow: row nowrap;
   justify-content: start;
@@ -63,7 +79,6 @@ const FormElement = styled.div`
 `;
 
 const Label = styled.label`
-  margin: 0 10px 0 0;
   width: 120px;
 `;
 
@@ -82,7 +97,7 @@ const ButtonContainer = styled.div`
 
 const SubmitButton = styled.button`
   height: 40px;
-  width: 40%;
+  width: 50%;
   max-width: 140px;
   margin: 30px 0;
   font-size: 1.2em;
@@ -104,9 +119,7 @@ const Footer = styled.div`
   height: fit-content;
   width: 80vw;
   max-width: 320px;
-  font-size: 0.9em;
-  color: white;
-  background: black;
+  color: #FDFD9B;
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
@@ -155,121 +168,123 @@ const NewListing = () => {
   return (
     <NewListingContainer>
       <Header>
-        <HeaderText>Create A New Listing</HeaderText>
+        <span>Create A New Listing</span>
       </Header>
 
-      <Intro>
-        Every year thousands of people get a great deal on their (tiny) cars
-        using our service. Simply fill in the form below and wait for the emails
-        to come in.
-      </Intro>
+      <MainContainer>
+        <Intro>
+          Every year thousands of people get a great deal on their (tiny) cars
+          using our service. Simply fill in the form below and wait for the
+          emails to come in.
+        </Intro>
 
-      <FormStyles onSubmit={handleAddListing}>
-        <FormElement>
-          <Label htmlFor="make">Make</Label>
-          <input
-            type="text"
-            id="make"
-            name="make"
-            placeholder=""
-            value={fields.make}
-            onChange={handleFieldChange}
-            required
-          />
-        </FormElement>
+        <FormStyles onSubmit={handleAddListing}>
+          <FormElement>
+            <Label htmlFor="make">Make</Label>
+            <input
+              type="text"
+              id="make"
+              name="make"
+              placeholder=""
+              value={fields.make}
+              onChange={handleFieldChange}
+              required
+            />
+          </FormElement>
 
-        <FormElement>
-          <Label htmlFor="model">Model</Label>
-          <input
-            type="text"
-            id="model"
-            name="model"
-            placeholder=""
-            value={fields.model}
-            onChange={handleFieldChange}
-            required
-          />
-        </FormElement>
+          <FormElement>
+            <Label htmlFor="model">Model</Label>
+            <input
+              type="text"
+              id="model"
+              name="model"
+              placeholder=""
+              value={fields.model}
+              onChange={handleFieldChange}
+              required
+            />
+          </FormElement>
 
-        <FormElement>
-          <Label htmlFor="year">Year</Label>
-          <NumberInput
-            id="year"
-            name="year"
-            placeholder=""
-            value={fields.year}
-            type="number"
-            min="1886"
-            step="1"
-            onWheel={(event) => event.preventDefault()}
-            onChange={handleFieldChange}
-            required
-          />
-        </FormElement>
+          <FormElement>
+            <Label htmlFor="year">Year</Label>
+            <NumberInput
+              id="year"
+              name="year"
+              placeholder=""
+              value={fields.year}
+              type="number"
+              min="1886"
+              step="1"
+              onWheel={(event) => event.preventDefault()}
+              onChange={handleFieldChange}
+              required
+            />
+          </FormElement>
 
-        <FormElement>
-          <Label htmlFor="price">Price (£)</Label>
-          <NumberInput
-            id="price"
-            name="price"
-            placeholder=""
-            value={fields.price}
-            type="number"
-            min="0"
-            step="1"
-            onWheel={(event) => event.preventDefault()}
-            onChange={handleFieldChange}
-            required
-          />
-        </FormElement>
+          <FormElement>
+            <Label htmlFor="price">Price (£)</Label>
+            <NumberInput
+              id="price"
+              name="price"
+              placeholder=""
+              value={fields.price}
+              type="number"
+              min="0"
+              step="1"
+              onWheel={(event) => event.preventDefault()}
+              onChange={handleFieldChange}
+              required
+            />
+          </FormElement>
 
-        <FormElement>
-          <Label htmlFor="location">Location</Label>
-          <input
-            type="text"
-            id="location"
-            name="location"
-            placeholder=""
-            value={fields.location}
-            onChange={handleFieldChange}
-            required
-          />
-        </FormElement>
+          <FormElement>
+            <Label htmlFor="location">Location</Label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              placeholder=""
+              value={fields.location}
+              onChange={handleFieldChange}
+              required
+            />
+          </FormElement>
 
-        <FormElement>
-          <Label htmlFor="email">Email:</Label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="example@mail.com"
-            value={fields.email}
-            onChange={handleFieldChange}
-            required
-          />
-        </FormElement>
+          <FormElement>
+            <Label htmlFor="email">Email:</Label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="example@mail.com"
+              value={fields.email}
+              onChange={handleFieldChange}
+              required
+            />
+          </FormElement>
 
-        <FormElement>
-          <Label htmlFor="authorization">Authorization:</Label>
-          <input
-            type="authorization"
-            id="authorization"
-            name="authorization"
-            placeholder="********"
-            value={fields.authorization}
-            onChange={handleFieldChange}
-            required
-          />
-        </FormElement>
+          <FormElement>
+            <Label htmlFor="authorization">Authorization:</Label>
+            <input
+              type="authorization"
+              id="authorization"
+              name="authorization"
+              placeholder="********"
+              value={fields.authorization}
+              onChange={handleFieldChange}
+              required
+            />
+          </FormElement>
 
-        <ButtonContainer>
-          <SubmitButton type="submit">
-            <p>Submit Listing!</p>
-          </SubmitButton>
-        </ButtonContainer>
+          <ButtonContainer>
+            <SubmitButton type="submit">
+              <p>Submit Listing!</p>
+            </SubmitButton>
+          </ButtonContainer>
 
-        <Footer>{alert}</Footer>
-      </FormStyles>
+          <Footer>{alert}</Footer>
+        </FormStyles>
+      </MainContainer>
     </NewListingContainer>
   );
 };
