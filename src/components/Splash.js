f89ba85d6e82bf45698a46997397618b18766d91
GummiCode <gummicodeblog@gmail.com>
2020-08-29 14:12:02 +0100

    Removed some styled components from the Splash component. Created a new component in the library directory called SplashButtopns to hold these components. Imported SplashButtons into Splash component.

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
 import { up } from 'styled-breakpoints';
-import { Link } from 'react-router-dom';
 import breakpoints from '../styles/breakpoints';
 import Buzzphrase from './library/Buzzphrase';
 import Modelphrase from './library/Modelphrase';
+import SplashButtons from './library/SplashButtons';
 import frameYellow from '../assets/images/FrameYellowB.png';
 import road1 from '../assets/images/roads/road1.png';
 
@@ -56,51 +56,6 @@ const FeaturedCar = styled.div`
   }
 `;
 
-const ButtonContainer = styled.div`
-  height: 40px;
-  width: 100%;
-  padding: 5px 0 0 0;
-  max-width: 360px;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-`;
-
-const NavButton = styled(Link)`
-  text-decoration: none;
-  height: 40px;
-  width: 40%;
-  max-width: 140px;
-  font-size: 1.2em;
-  color: black;
-  font-family: 'Contrail One', Helvetica, sans-serif;
-  font-weight: bold;
-  background: whitesmoke;
-  border-radius: 10px;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-
-  &:visited {
-    background: whitesmoke;
-    color: black;
-    text-decoration: none;
-  }
-
-  &:active {
-    background: whitesmoke;
-    color: black;
-    text-decoration: none;
-  }
-
-  &:hover {
-    background: whitesmoke;
-    color: black;
-    text-decoration: none;
-  }
-`;
-
 const LoadingMessage = styled.h2`
   width: 100vw;
   margin: 120px 0 0 0;
@@ -127,10 +82,7 @@ const Splash = ({ selectedListing, buzzState }) => {
             make={selectedListing.make}
             model={selectedListing.model}
           />
-          <ButtonContainer>
-            <NavButton to="/Listing">View listing!</NavButton>
-            <NavButton to="/AllListings">All Cars</NavButton>
-          </ButtonContainer>
+          <SplashButtons />
         </MainContainer>
       </SplashContainer>
     </ThemeProvider>
---
+++ src/components/library/SplashButtons.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const ButtonContainer = styled.div`
+  height: 40px;
+  width: 100%;
+  padding: 5px 0 0 0;
+  max-width: 360px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+`;
+
+const NavButton = styled(Link)`
+  text-decoration: none;
+  height: 40px;
+  width: 40%;
+  max-width: 140px;
+  font-size: 1.2em;
+  color: black;
+  font-family: 'Contrail One', Helvetica, sans-serif;
+  font-weight: bold;
+  background: whitesmoke;
+  border-radius: 10px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  &:visited {
+    background: whitesmoke;
+    color: black;
+    text-decoration: none;
+  }
+
+  &:active {
+    background: whitesmoke;
+    color: black;
+    text-decoration: none;
+  }
+
+  &:hover {
+    background: whitesmoke;
+    color: black;
+    text-decoration: none;
+  }
+`;
+
+const SplashButtons = () => {
+  return (
+    <ButtonContainer>
+      <NavButton to="/Listing">View listing!</NavButton>
+      <NavButton to="/AllListings">All Cars</NavButton>
+    </ButtonContainer>
+  );
+};
+
+export default SplashButtons;
